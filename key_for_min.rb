21e5db23685b809b592f1662d78e6b4226d96aa9
JD Brewer Hofmann <justindhofmann@gmail.com>
2020-08-13 21:08:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,19 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  if name_hash == {}
-    return nil
-  else
-    min_item = ""
-    name_hash.each do 
-      |item, price |
-      
-      
-      # compare price of each item to what?
-        # set 
-      # find the lowest price
-      
-      # return key for lowest price
-    end
+
+  min_item = ""
+  name_hash.each do 
+    |item, price |
+    
+    
+    # compare price of each item to what?
+      # set 
+    # find the lowest price
+    
+    # return key for lowest price
   end
 end
