2f0b0e00dee52758faab84b63b1b7408531a0e6e
JD Brewer Hofmann <justindhofmann@gmail.com>
2020-08-13 21:14:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,8 @@ def key_for_min_value(name_hash)
 
   lowest_key = nil 
   lowest_value = nil 
-  name_hash.each do 
-    |k, v |
-    
-    if lowest_value == nil || lowest_value < v 
+  name_hash.each do |k, v |
+    if lowest_value == nil || lowest_value > v 
     lowest_key = k 
     lowest_value = v 
   end
