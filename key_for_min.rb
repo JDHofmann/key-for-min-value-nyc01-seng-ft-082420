13db2935d942593c8b06a46e4161635876286527
JD Brewer Hofmann <justindhofmann@gmail.com>
2020-08-13 20:40:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,11 @@
 def key_for_min_value(name_hash)
   name_hash.each do 
     |item, price |
+    
     # compare price of each item 
-   
+    
+    # find the lowest price
+    
+    # return key for lowest price
   end
 end
