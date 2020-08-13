8e4f0ee0783d0222009d6b1f92379277a7055c12
JD Brewer Hofmann <justindhofmann@gmail.com>
2020-08-13 21:14:27 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,13 @@
 
 def key_for_min_value(name_hash)
 
+  lowest_key = nil 
+  lowest_value = nil 
+  name_hash.each do |k, v |
+    if lowest_value == nil || lowest_value > v 
+    lowest_key = k 
+    lowest_value = v 
+  end
+  end
+  lowest_key
 end
