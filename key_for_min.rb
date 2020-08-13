d535dc56e0e5cd68e0c1a7168fcd91ae8bfda6a7
JD Brewer Hofmann <justindhofmann@gmail.com>
2020-08-13 20:51:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,14 +5,16 @@ def key_for_min_value(name_hash)
   if name_hash == {}
     return nil
   else
-  name_hash.each do 
-    |item, price |
-    
-    # compare price of each item to what?
-      # set 
-    # find the lowest price
-    
-    # return key for lowest price
-  end
+    min_item = ""
+    name_hash.each do 
+      |item, price |
+      
+      
+      # compare price of each item to what?
+        # set 
+      # find the lowest price
+      
+      # return key for lowest price
+    end
   end
 end
